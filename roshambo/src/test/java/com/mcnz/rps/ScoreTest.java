132c9c8a599789db8db6794002e4f5cccfa08731
Maven User <Maven User@192.168.204.133>
2018-05-04 18:17:16 -0400

    jacoco test running with hash code correct source file error fixed

--- roshambo/src/test/java/com/mcnz/rps/ScoreTest.java
+++ roshambo/src/test/java/com/mcnz/rps/ScoreTest.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 
 public class ScoreTest {
 
-	@Test
-	public void testIncreaseWins() {
-		Score score = new Score();
-		score.increaseWins();
-		Assert.assertTrue(score.getWins()>0);
-	}
+//	@Test
+//	public void testIncreaseWins() {
+//		Score score = new Score();
+//		score.increaseWins();
+//		Assert.assertTrue(score.getWins()>0);
+//	} 
 	@Test
 	public void testIncreaseLosses() {
 		Score score = new Score();
@@ -23,6 +23,12 @@ public class ScoreTest {
 		Score score = new Score();
 		score.increaseTies();
 		Assert.assertTrue(score.getTies()>0);
+	}
+	
+	@Test
+	public void testToString() {
+		Score score = new Score();
+		Assert.assertTrue(score.toString().length()>0);
 	}
 
 }
