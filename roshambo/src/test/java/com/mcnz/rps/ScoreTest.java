b6ace72e07df589974ad74457f20c9a5eabd619d
wpsadmin <wpsadmin@192.168.204.133>
2018-05-04 16:03:49 -0400

    created skeleton for ScoreTest class

---
+++ roshambo/src/test/java/com/mcnz/rps/ScoreTest.java
@@ -0,0 +1,20 @@
+package com.mcnz.rps;
+
+import junit.framework.TestCase;
+
+public class ScoreTest extends TestCase {
+
+
+	public void testIncreaseWins() {
+		fail("Not yet implemented");
+	}
+
+	public void testIncreaseLosses() {
+		fail("Not yet implemented");
+	}
+
+	public void testIncreaseTies() {
+		fail("Not yet implemented");
+	}
+
+}
