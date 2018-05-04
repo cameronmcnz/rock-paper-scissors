a507e132a8dc61edf387c1d1e82cbfefef492bae
Maven User <Maven User@192.168.204.133>
2018-05-04 19:09:07 -0400

    added GameSummaryTest and successfully ran test cases

--- roshambo/src/main/java/com/mcnz/rps/GameSummary.java
+++ roshambo/src/main/java/com/mcnz/rps/GameSummary.java
@@ -7,32 +7,24 @@ public class GameSummary {
 	String result;
 	java.util.Date date;
 	
-	public GameSummary(){}
-
 	public String getClientGesture() {
 		return clientGesture;
 	}
-	public void setClientGesture(String clientGesture) {
-		this.clientGesture = clientGesture;
-	}
+
 	public String getServerGesture() {
 		return serverGesture;
 	}
-	public void setServerGesture(String serverGesture) {
-		this.serverGesture = serverGesture;
-	}
+
 	public String getResult() {
 		return result;
 	}
-	public void setResult(String result) {
-		this.result = result;
-	}
+
 	public java.util.Date getDate() {
 		return date;
 	}
-	public void setDate(java.util.Date date) {
-		this.date = date;
-	}
+	
+	public GameSummary() {};
+
 	public GameSummary(String clientGesture, String serverGesture, String result) {
 		super();
 		this.clientGesture = clientGesture;
