125b6edd2b19541ed722bdb9b181a60de08498b6
learn-devops-fast <41976996+learn-devops-fast@users.noreply.github.com>
2018-11-09 18:19:19 -0500

    Create install-all.sh

---
+++ install-all.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+#stop any running services
+sudo service apache2 stop
+sudo service jenkins stop
+sudo service nginx stop
+
+#Java
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt update
+sudo apt install oracle-java8-installer -y
+
+#Git and Maven
+sudo apt update
+sudo apt install git-all -y
+sudo apt-get install maven
+git config --global user.name "Triple Baconator"
+git config --global user.email triple@baconator.on.ca
+
+#Jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+
+#Docker
+apt-get update
+apt-get -y install docker.io
+ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+update-rc.d docker.io defaults
+docker pull ubuntu
+docker run -i -t ubuntu /bin/bash
+docker search debian
+
+#Artifactory
+
+wget https://bintray.com/jfrog/artifactory/download_file?file_path=jfrog-artifactory-oss-6.5.2.zip
+# unzip jfrog-artifactory-oss-6.5.2.zip
+unzip download_file?file_path=jfrog-artifactory-oss-6.5.2.zip
+cd art*
+cd bin*
+sudo ./installService.sh
+sudo service artifactory start
