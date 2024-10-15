43b0ffb48630be2b8ae8f806be6ecafdab30338d
Cameron McKenzie <38986973+cameronmcnz@users.noreply.github.com>
2024-10-15 16:04:42 -0400

    Update Game.java

--- src/main/java/com/mcnz/rps/Game.java
+++ src/main/java/com/mcnz/rps/Game.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 import javax.swing.*;
 
 public class Game {
-	
+	String password = "123456";
 	public static void main(String[] args) {
 		String prompt = "Will it be rock, paper or scissors?";
 		String input = JOptionPane.showInputDialog(prompt);
